1ef3bba9891d4d197ed78d92a9ec202cd73ccbb8
ruairi <ruairitobrien@gmail.com>
2014-01-27 00:20:55 +0000

    added some input detection

--- src/nathaniel.cc
+++ src/nathaniel.cc
@@ -1,123 +1,226 @@
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
 #include <string>
+
+#define CONTENT_DIR "../content"
 
 //Screen attributes 
 const int SCREEN_WIDTH = 640; 
 const int SCREEN_HEIGHT = 480; 
 const int SCREEN_BPP = 32; 
-//The surfaces 
-SDL_Surface *image = NULL; 
+
+//The surfaces
+SDL_Surface *background = NULL;
+SDL_Surface *upMessage = NULL;
+SDL_Surface *downMessage = NULL;
+SDL_Surface *leftMessage = NULL;
+SDL_Surface *rightMessage = NULL;
+SDL_Surface *message = NULL;
 SDL_Surface *screen = NULL;
-//The event structure that will be used 
+
+//The event structure
 SDL_Event event;
 
-SDL_Surface *load_image( std::string filename ) 
-{ 
-	//The image that's loaded 
-	SDL_Surface* loadedImage = NULL; 
-	//The optimized image that will be used 
-	SDL_Surface* optimizedImage = NULL; 
-	//Load the image 
-	loadedImage = IMG_Load( filename.c_str() ); 
-	//If the image loaded 
-	if( loadedImage != NULL ) 
-	{ 
-		//Create an optimized image 
-		optimizedImage = SDL_DisplayFormat( loadedImage ); 
-		//Free the old image 
-		SDL_FreeSurface( loadedImage ); 
-	} 
-		//Return the optimized image 
-	return optimizedImage; 
-} 
+//The font
+TTF_Font *font = NULL;
 
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination ) 
-{ 
-	//Temporary rectangle to hold the offsets 
-	SDL_Rect offset; 
-	//Get the offsets 
-	offset.x = x; offset.y = y; 
-	//Blit the surface 
-	SDL_BlitSurface( source, NULL, destination, &offset ); 
-}
+//The color of the font
+SDL_Color textColor = { 255, 255, 255 };
 
-bool load_files() 
-{ 
-	//Load the image 
-	image = load_image( "test.bmp" ); 
-	//If there was an error in loading the image 
-	if( image == NULL ) 
-	{ 
-		return false; 
-	} 
-	//If everything loaded fine return 
-	true; 
-} 
-
-bool init() { 
-	//Initialize all SDL subsystems 
-	if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) 
-	{ 
-		return false; 
-	} 
-	//Set up the screen 
-	screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE ); 
-	//If there was an error in setting up the screen 
-	if( screen == NULL ) 
-	{ 
-		return false; 
-	} 
-	//Set the window caption 
-	SDL_WM_SetCaption( "Event test", NULL ); 
-	//If everything initialized fine 
-	return true; 
-}
-
-void clean_up() 
-{ 
-	//Free the image 
-	SDL_FreeSurface( image ); 
-	//Quit SDL 
-	SDL_Quit(); 
-}
-
-int main( int argc, char* arg[] )
+SDL_Surface *load_image( std::string filename )
 {
-	bool quit = false;
-	//Initialize 
-	if( init() == false ) 
-	{ 
-		return 1; 
-	} 
-	//Load the files 
-	if( load_files() == false ) 
-	{ 
-		return 1; 
-	} 
+    //The image that's loaded
+    SDL_Surface* loadedImage = NULL;
 
-	//Apply the surface to the screen 
-	apply_surface( 0, 0, image, screen ); 
-	//Update the screen 
-	if( SDL_Flip( screen ) == -1 ) 
-	{ 
-		return 1; 
-	}
-	 //While the user hasn't quit 
-	while( quit == false ) 
-	{
-		//While there's an event to handle 
-		while( SDL_PollEvent( &event ) ) 
-		{
-			//If the user has Xed out the window 
-			if( event.type == SDL_QUIT ) 
-			{ 
-				//Quit the program 
-				quit = true; 
-			} 
-		} 
-	}
-	 //Free the surface and quit SDL 
-	clean_up(); 
-	return 0;
+    //The optimized surface that will be used
+    SDL_Surface* optimizedImage = NULL;
+
+    //Load the image
+    loadedImage = IMG_Load( filename.c_str() );
+
+    //If the image loaded
+    if( loadedImage != NULL )
+    {
+        //Create an optimized surface
+        optimizedImage = SDL_DisplayFormat( loadedImage );
+
+        //Free the old surface
+        SDL_FreeSurface( loadedImage );
+
+        //If the surface was optimized
+        if( optimizedImage != NULL )
+        {
+            //Color key surface
+            SDL_SetColorKey( optimizedImage, SDL_SRCCOLORKEY, SDL_MapRGB( optimizedImage->format, 0, 0xFF, 0xFF ) );
+        }
+    }
+
+    //Return the optimized surface
+    return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination, SDL_Rect* clip = NULL )
+{
+    //Holds offsets
+    SDL_Rect offset;
+
+    //Get offsets
+    offset.x = x;
+    offset.y = y;
+
+    //Blit
+    SDL_BlitSurface( source, clip, destination, &offset );
+}
+
+bool init()
+{
+    //Initialize all SDL subsystems
+    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        return false;
+    }
+
+    //Set up the screen
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+
+    //If there was an error in setting up the screen
+    if( screen == NULL )
+    {
+        return false;
+    }
+
+    //Initialize SDL_ttf
+    if( TTF_Init() == -1 )
+    {
+        return false;
+    }
+
+    //Set the window caption
+    SDL_WM_SetCaption( "Press an Arrow Key", NULL );
+
+    //If everything initialized fine
+    return true;
+}
+
+bool load_files()
+{
+    //Load the background image
+    background = load_image( CONTENT_DIR"/test.bmp" );
+
+    //Open the font
+    font = TTF_OpenFont( CONTENT_DIR"/fonts/lazy.ttf", 72 );
+
+    //If there was a problem in loading the background
+    if( background == NULL )
+    {
+        return false;
+    }
+
+    //If there was an error in loading the font
+    if( font == NULL )
+    {
+        return false;
+    }
+
+    //If everything loaded fine
+    return true;
+}
+
+void clean_up()
+{
+    //Free the surfaces
+    SDL_FreeSurface( background );
+    SDL_FreeSurface( upMessage );
+    SDL_FreeSurface( downMessage );
+    SDL_FreeSurface( leftMessage );
+    SDL_FreeSurface( rightMessage );
+
+    //Close the font
+    TTF_CloseFont( font );
+
+    //Quit SDL_ttf
+    TTF_Quit();
+
+    //Quit SDL
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+    //Quit flag
+    bool quit = false;
+
+    //Initialize
+    if( init() == false )
+    {
+        return 1;
+    }
+
+    //Load the files
+    if( load_files() == false )
+    {
+        return 1;
+    }
+
+    //Generate the message surfaces
+    upMessage = TTF_RenderText_Solid( font, "Up was pressed.", textColor );
+    downMessage = TTF_RenderText_Solid( font, "Down was pressed.", textColor );
+    leftMessage = TTF_RenderText_Solid( font, "Left was pressed", textColor );
+    rightMessage = TTF_RenderText_Solid( font, "Right was pressed", textColor );
+
+    //Apply the background
+    apply_surface( 0, 0, background, screen );
+
+    //While the user hasn't quit
+    while( quit == false )
+    {
+        //If there's an event to handle
+        if( SDL_PollEvent( &event ) )
+        {
+            //If a key was pressed
+            if( event.type == SDL_KEYDOWN )
+            {
+                //Set the proper message surface
+                switch( event.key.keysym.sym )
+                {
+                    case SDLK_UP: message = upMessage; break;
+                    case SDLK_DOWN: message = downMessage; break;
+                    case SDLK_LEFT: message = leftMessage; break;
+                    case SDLK_RIGHT: message = rightMessage; break;
+                }
+            }
+
+            //If the user has Xed out the window
+            else if( event.type == SDL_QUIT )
+            {
+                //Quit the program
+                quit = true;
+            }
+        }
+
+        //If a message needs to be displayed
+        if( message != NULL )
+        {
+            //Apply the background to the screen
+            apply_surface( 0, 0, background, screen );
+
+            //Apply the message centered on the screen
+            apply_surface( ( SCREEN_WIDTH - message->w ) / 2, ( SCREEN_HEIGHT - message->h ) / 2, message, screen );
+
+            //Null the surface pointer
+            message = NULL;
+        }
+
+        //Update the screen
+        if( SDL_Flip( screen ) == -1 )
+        {
+            return 1;
+        }
+    }
+
+    //Clean up
+    clean_up();
+
+    return 0;
 }
