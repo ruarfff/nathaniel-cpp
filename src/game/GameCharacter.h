b616b4fb79f493014f23be79b613d8742896e6c8
ruairi <ruairitobrien@gmail.com>
2014-02-02 20:44:59 +0000

    Updated GameCharacter header class definition

--- src/game/GameCharacter.h
+++ src/game/GameCharacter.h
@@ -1,5 +1,26 @@
 #ifndef _GAMECHARACTER_H
 #define _GAMECHARACTER_H
 
+#include "SDL/SDL.h"
+
+class GameCharacter
+{
+	private:
+	
+	int offSet;
+	int velocity;
+	int frame;
+	int status;
+
+	const int WIDTH = 64;
+	const int HEIGHT = 205;
+
+	public:
+
+	GameCharacter();
+	void handleEvents();
+	void move();
+	void show();
+};
 
 #endif
